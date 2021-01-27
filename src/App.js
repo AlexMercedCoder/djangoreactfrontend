58e3fc18f61c7a74101eb2861f0f7d9674cd32d7
Alex Merced <willcodeforfood@alexmerced.com>
2021-01-27 16:41:27 -0500

    finish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route} from "react-router-dom"
+import React from "react"
+import Login from "./pages/Login"
+import Todos from "./pages/todos"
 
-function App() {
+function App(props) {
+  
+  const [token, setToken] = React.useState({})
+
+  const URL = "https://djangoreactbackendam.herokuapp.com/"
+
+  const getToken = async (un, pw) => {
+    const response = await fetch(URL + "api/token/", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({username: un, password: pw})
+    })
+    const data = await response.json()
+    console.log(data)
+    setToken(data)
+    localStorage.setItem("token", JSON.stringify(data))
+  }
+
+  React.useEffect(() => {
+    const possibleToken = JSON.parse(localStorage.getItem("token"))
+    if (possibleToken){
+      setToken(possibleToken)
+    }
+  }, [])
+
+  React.useEffect(() => {
+    if(token.access){
+      props.history.push("/todos")
+    } else {
+      props.history.push("/")
+    }
+  }, [token.access])
+  
   return (
     <div className="App">
-  <h1>Hello World</h1>
+  <h1>My TOdo List</h1>
+
+
+
+  <Route exact path="/" render={(rp) => <Login getToken={getToken} {...rp}/>}/>
+
+  <Route path="/todos" render={(rp) => <Todos tokens={token} URL={URL} {...rp}/>}/>
     </div>
   );
 }
